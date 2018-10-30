8831b40d98508e86aeab174b0977f84d71d03fe2
Mariem BB <maryembenbelgacem@hotmail.fr>
2018-10-30 23:44:40 +0100

    refs#library-managment-v2

--- src/main/java/fr/d2factory/libraryapp/library/HasLateBooksException.java
+++ src/main/java/fr/d2factory/libraryapp/library/HasLateBooksException.java
@@ -1,7 +1,13 @@
 package fr.d2factory.libraryapp.library;
 
 /**
- * This exception is thrown when a member who owns late books tries to borrow another book
+ * This exception is thrown when a member who owns late books tries to borrow
+ * another book
  */
 public class HasLateBooksException extends RuntimeException {
+
+	public HasLateBooksException(String error_message) {
+		super(error_message);
+	}
+
 }
