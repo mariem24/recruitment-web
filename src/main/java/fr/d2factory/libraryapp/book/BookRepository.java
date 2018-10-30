891b63fcaae26db0251bb0e5c10648f28c0c5ae6
Mariem BB <maryembenbelgacem@hotmail.fr>
2018-10-30 23:30:25 +0100

    refs#library-managment-v1

--- src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
+++ src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
@@ -9,40 +9,38 @@ import java.util.Map;
  * The book repository emulates a database via 2 HashMaps
  */
 public class BookRepository {
-    private Map<ISBN, Book> availableBooks = new HashMap<>();
-    private Map<Book, LocalDate> borrowedBooks = new HashMap<>();
+	private Map<ISBN, Book> availableBooks = new HashMap<>();
+	private Map<Book, LocalDate> borrowedBooks = new HashMap<>();
 
-    public void addBooks(List<Book> books){
-        for (Book book :books){
-        	availableBooks.put(book.isbn, book);
-        }
-    }
+	public void addBooks(List<Book> books) {
+		books.forEach(book->availableBooks.put(book.isbn, book));
+	}
 
-    public Book findBook(long isbnCode) {
-    	//Search the book in the available ones 
-      for(Map.Entry<ISBN, Book> entry : availableBooks.entrySet()) {
-        long key = entry.getKey().isbnCode; 
-        if (key==isbnCode){
-            return entry.getValue();
-        }
-       }   
-        return null;
-    }
+	public Book findBook(long isbnCode) {
+		// Search the book in the available ones
+		for (Map.Entry<ISBN, Book> entry : availableBooks.entrySet()) {
+			long key = entry.getKey().isbnCode;
+			if (key == isbnCode) {
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
 
-    public void saveBookBorrow(Book book, LocalDate borrowedAt){
-    	// put the book in the borrowed books 
-        borrowedBooks.put(book,borrowedAt);
-        // Remove book from the available books
-        availableBooks.remove(book.isbn);
-    }
+	public void saveBookBorrow(Book book, LocalDate borrowedAt) {
+		// put the book in the borrowed books
+		borrowedBooks.put(book, borrowedAt);
+		// Remove book from the available books
+		availableBooks.remove(book.isbn);
+	}
 
-    public LocalDate findBorrowedBookDate(Book book) {
-        for(Map.Entry<Book, LocalDate> entry : borrowedBooks.entrySet()) {
-        Book key = entry.getKey(); 
-        if (key.equals(book)){
-            return entry.getValue();
-        }
-       }
-        return null;
-    }
+	public LocalDate findBorrowedBookDate(Book book) {
+		for (Map.Entry<Book, LocalDate> entry : borrowedBooks.entrySet()) {
+			Book key = entry.getKey();
+			if (key.equals(book)) {
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
 }
